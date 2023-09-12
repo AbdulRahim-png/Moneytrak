fdfdb72557658a8540d80082746c9fba0846bd09
AbdulRahim-png <abdulrahimsheikfaisal@gmail.com>
2023-09-11 20:47:42 -0700

    New assig

--- NewDom.js
+++ NewDom.js
@@ -2,3 +2,14 @@ console.log(document.all)
 console.log(document.getElementById('main-header'))
 var headerTitle = document.getElementById('header-title')
 console.log(headerTitle)
+var items =document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+items[1].textContent = "Hello 2"
+items[3].computedStyleMap.backgroundColor = 'yellow';
+items[1].style.fontWeight = 'bold'
+items[1].style.backgroundColor = 'yellow'
+items[2].style.backgroundColor = 'green'
+items[2].style.fontWeight = 'bold'
+items[0].style.fontWeight = 'bold'
+items[3].style.fontWeight = 'bold'
--- Sdom.js
+++
@@ -1,4 +0,0 @@
-console.log(document.all)
-console.log(document.getElementById('main-header'))
-var headerTitle = document.getElementById('header-title')
-console.log(headerTitle)
